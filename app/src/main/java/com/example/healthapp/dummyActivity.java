254f563866f0e7eccb7bc2bd3041a459d581f59b
Dina-Shanjida <dinasanjida0210@gmail.com>
2025-04-26 16:37:55 +0600

    2100 software project

---
+++ app/src/main/java/com/example/healthapp/dummyActivity.java
@@ -0,0 +1,41 @@
+package com.example.healthapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class dummyActivity extends AppCompatActivity {
+
+    Button btndummy  ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dummy);
+
+        btndummy = findViewById(R.id.button);
+
+        btndummy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "congo" , Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(dummyActivity.this , LoginActivity.class));
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
